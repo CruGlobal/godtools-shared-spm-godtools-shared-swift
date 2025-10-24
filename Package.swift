c135470da795b53ad90dc387e383fc48464b5389
levieggertcru <levi.eggert@cru.org>
2025-10-24 11:57:34 +0000

    Update package version

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "GodToolsShared",
+  platforms: [
+    .iOS(.v16),
+  ],
+  products: [
+      .library(name: "GodToolsShared", targets: ["GodToolsShared"])
+  ],
+  dependencies: [],
+  targets: [
+      .binaryTarget(
+        name: "GodToolsShared",
+        url: "https://cruglobal.jfrog.io/artifactory/swift-snapshots-local/CruGlobal/GodToolsShared/",
+        checksum: ""
+      )
+  ]
+)
